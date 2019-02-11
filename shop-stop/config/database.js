11a22964d3479f4eeb4a636361027bff374d7095
peterborisov <borisov.peter@yahoo.co.uk>
2019-02-11 20:31:14 +0200

    add db

--- shop-stop/config/database.js
+++ shop-stop/config/database.js
@@ -1,18 +1,35 @@
-let products = [];
-let count = 1;
+const FS = require('fs');
+const PATH = require('path');
+const DB_PATH = PATH.join(__dirname, '/database.json');
+
+function getProducts() {
+    if (FS.existsSync(DB_PATH) === false) {
+        FS.writeFileSync(DB_PATH, '[]');
+        return [];
+    }
+
+    let json = FS.readFileSync(DB_PATH).toString() || '[]';
+    return JSON.parse(json);
+}
+
+function saveProducts(products) {
+    let json = JSON.stringify(products);
+    FS.writeFileSync(DB_PATH, json);
+}
 
 module.exports.products = {};
 
-module.exports.products.getAll = () => {
-    return products;
-};
+module.exports.products.getAll = getProducts;
 
 module.exports.products.add = (product) => {
-    product.id = count++;
+    let products = getProducts();
+    product.id = products.length + 1;
     products.push(product);
+    saveProducts(products);
 };
 
 module.exports.products.findByName = (name) => {
+    let products = getProducts();
     for (let product of products) {
         if (product.name === name) {
             return product;
@@ -24,6 +41,7 @@ module.exports.products.findByName = (name) => {
 
 module.exports.products.filterByQuery = (query) => {
     let result = [];
+    let products = getProducts();
 
     for (let product of products) {
         if (product.name.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
